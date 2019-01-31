c5dd769226065453112f761c75d477091472869e
jamuna88 <jamunadevananth@gmail.com>
2019-01-31 10:42:07 +0530

    sixth commit

--- src/firstmodule/firstclass.java
+++ src/firstmodule/firstclass.java
@@ -5,7 +5,9 @@ public class firstclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi im jamuna tis is my first  project");
-                System.out.println("Hi im jamuna tis is my second java project");
+                System.out.println("Hi im jamuna tis is my second  project");
+                System.out.println("Hi im jamuna tis is my third project");
+
 
 	}
 
