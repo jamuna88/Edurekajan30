5469976a7aae2477d8452b9bcae2e73dcb6bd835
jamuna88 <jamunadevananth@gmail.com>
2019-01-31 09:43:33 +0530

    First commit

---
+++ src/firstmodule/firstclass.java
@@ -0,0 +1,11 @@
+package firstmodule;
+
+public class firstclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi im jamuna tis is my first java project");
+
+	}
+
+}
---
+++ src/secondmodule/seleniumcommands.java
@@ -0,0 +1,15 @@
+package secondmodule;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class seleniumcommands {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		
+
+	}
+
+}
